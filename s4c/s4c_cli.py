69822ff105d90e04641a8ae79d4f7fd90b1db171
Gabriele Rimoli <109908086+jgabaut@users.noreply.github.com>
2026-08-02 16:34:06 +0200

    feat: 0.2.4 fix s4c-file mode output (#25)
    ### Changed
    
    - Fix `s4c-file` mode output

--- s4c/s4c_cli.py
+++ s4c/s4c_cli.py
@@ -32,7 +32,7 @@
 #
 # @section author_s4c_cli Author(s)
 # - Created by jgabaut on 02/01/2024.
-# - Modified by jgabaut on 29/09/2025.
+# - Modified by jgabaut on 27/07/2026.
 
 import sys
 import os
@@ -42,7 +42,7 @@ from .core.sprites import main as sprites_main
 from .core.sheet_converter import main as sheet_converter_main
 from .core.png_resize import main as png_resize_main
 
-S4C_CLI_VERSION = "0.2.3"
+S4C_CLI_VERSION = "0.2.4"
 
 EXPECTED_S4C_ANIMATE_V = "0.5.0"
 
